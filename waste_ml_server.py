ff461522138fcd819bef10cc216f1b37dd88d0f4
vineet <Mastergenos228@gmail.com>
2025-06-30 16:25:07 +0530

    initial commit

--- waste_ml_server.py
+++ waste_ml_server.py
@@ -75,4 +75,4 @@ async def classify_waste(file: UploadFile = File(...)):
     
 import uvicorn
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
